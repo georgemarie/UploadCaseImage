3012baa25727cc0393b583fabb7522271aaa9697
Abdelrahman Gamal <abdelrahman.khalaf@evapharma.com>
2024-08-19 00:56:25 +0300

    fix GlobalExceptionHandler

--- UploadingCaseImages/Program.cs
+++ UploadingCaseImages/Program.cs
@@ -70,14 +70,12 @@ if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
-	app.UseDeveloperExceptionPage();
 }
 else
 {
-	app.UseExceptionHandler("/Home/Error");
 	app.UseHsts();
 }
-
+app.UseExceptionHandler();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
